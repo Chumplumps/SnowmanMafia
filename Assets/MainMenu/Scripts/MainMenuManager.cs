7198e41fd771e99b70df79dbe87b698d4ce781e8
josh <48034855+JDJoffe@users.noreply.github.com>
2019-08-11 20:31:42 +1000

    Mainmenustart

---
+++ Assets/MainMenu/Scripts/MainMenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public  class MainMenuManager : MonoBehaviour
+{
+    public TextMeshProUGUI antiAliasingText;
+    public int antiAliasingLevel;
+   public  void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void AntiAliasing()
+    {
+        QualitySettings.antiAliasing = antiAliasingLevel;
+        antiAliasingText.text = antiAliasingLevel.ToString();
+        antiAliasingLevel++;
+        if (antiAliasingLevel == 5)
+        {
+            antiAliasingLevel = 0;
+        }
+
+        
+       
+       
+
+    }
+}
